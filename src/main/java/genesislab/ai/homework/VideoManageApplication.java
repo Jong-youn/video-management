5a3621302270c6c2829db11c687a2c827c4b22ac
jongyounlee <jayylee.dev@gmail.com>
2023-09-05 12:47:53 +0900

    라이브러리 셋팅

---
+++ src/main/java/genesislab/ai/homework/VideoManageApplication.java
@@ -0,0 +1,13 @@
+package genesislab.ai.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoManageApplication.class, args);
+	}
+
+}
